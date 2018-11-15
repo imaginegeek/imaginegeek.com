a6543a14a9478e309a11513f313c325e0f35ee7d
Nicholas Abrams Work <nicholasabramswork@Nicholass-MacBook-Pro.local>
2018-11-15 15:21:40 -0500

    Fixed link errors throwing in console on user scroll.

--- js/navigation/navbar.js
+++ js/navigation/navbar.js
@@ -20,83 +20,84 @@
 	 * http://www.opensource.org/licenses/mit-license.php
 	 * 
 	 */
-	 
+
 	$(document).ready(function() {
-		$.fn.scrollTo = function( options ) {
+	    $.fn.scrollTo = function(options) {
 
-			var settings = {
-				offset : -127,       //an integer allowing you to offset the position by a certain number of pixels. Can be negative or positive
-				speed : 'slow',   //speed at which the scroll animates
-				override : null,  //if you want to override the default way this plugin works, pass in the ID of the element you want to scroll through here
-				easing : null //easing equation for the animation. Supports easing plugin as well (http://gsgd.co.uk/sandbox/jquery/easing/)
-			};
+	        var settings = {
+	            offset: -127, //an integer allowing you to offset the position by a certain number of pixels. Can be negative or positive
+	            speed: 'slow', //speed at which the scroll animates
+	            override: null, //if you want to override the default way this plugin works, pass in the ID of the element you want to scroll through here
+	            easing: null //easing equation for the animation. Supports easing plugin as well (http://gsgd.co.uk/sandbox/jquery/easing/)
+	        };
 
-			if (options) {
-				if(options.override){
-					//if they choose to override, make sure the hash is there
-					options.override = (override('#') != -1)? options.override:'#' + options.override;
-				}
-				$.extend( settings, options );
-			}
+	        if (options) {
+	            if (options.override) {
+	                //if they choose to override, make sure the hash is there
+	                options.override = (override('#') != -1) ? options.override : '#' + options.override;
+	            }
+	            $.extend(settings, options);
+	        }
 
-			return this.each(function(i, el){
-				$(el).click(function(e){
-					var idToLookAt;
-					if ($(el).attr('href').match(/#/) !== null) {
-						e.preventDefault();
-						idToLookAt = (settings.override)? settings.override:$(el).attr('href');//see if the user is forcing an ID they want to use
-						//if the browser supports it, we push the hash into the pushState for better linking later
-						if(history.pushState){
-							history.pushState(null, null, idToLookAt);
-							$('html,body').stop().animate({scrollTop: $(idToLookAt).offset().top + settings.offset}, settings.speed, settings.easing);
-						}else{
-							//if the browser doesn't support pushState, we set the hash after the animation, which may cause issues if you use offset
-							$('html,body').stop().animate({scrollTop: $(idToLookAt).offset().top + settings.offset}, settings.speed, settings.easing,function(e){
-								//set the hash of the window for better linking
-								window.location.hash = idToLookAt;
-							});
-						}
-					}
-				});
-			});
-		};
-		  
-		$('#GoToHome, .GoToto, #GoToWorks, #GoToFeatures, #GoToTeam, #GoToGallery, #GoToContact' ).scrollTo({ speed: 1400 });
-		
-		//Bootstraping variable
-		headerWrapper		= parseInt($('.navbar').height());
-		offsetTolerance	= 84;
-		
-		//Detecting user's scroll
-		$(window).scroll(function() {
-		
-			//Check scroll position
-			scrollPosition	= parseInt($(this).scrollTop());
-			
-			//Move trough each menu and check its position with scroll position then add selected class
-			$('.navbar-nav li a').each(function() {
+	        return this.each(function(i, el) {
+	            $(el).click(function(e) {
+	                var idToLookAt;
+	                if ($(el).attr('href').match(/#/) !== null) {
+	                    e.preventDefault();
+	                    idToLookAt = (settings.override) ? settings.override : $(el).attr('href'); //see if the user is forcing an ID they want to use
+	                    //if the browser supports it, we push the hash into the pushState for better linking later
+	                    if (history.pushState) {
+	                        history.pushState(null, null, idToLookAt);
+	                        $('html,body').stop().animate({ scrollTop: $(idToLookAt).offset().top + settings.offset }, settings.speed, settings.easing);
+	                    } else {
+	                        //if the browser doesn't support pushState, we set the hash after the animation, which may cause issues if you use offset
+	                        $('html,body').stop().animate({ scrollTop: $(idToLookAt).offset().top + settings.offset }, settings.speed, settings.easing, function(e) {
+	                            //set the hash of the window for better linking
+	                            window.location.hash = idToLookAt;
+	                        });
+	                    }
+	                }
+	            });
+	        });
+	    };
 
-				thisHref				= $(this).attr('href');
-				thisTruePosition	= parseInt($(thisHref).offset().top);
-				thisPosition 		= thisTruePosition - headerWrapper - offsetTolerance;
-				
-				if(scrollPosition >= thisPosition) {
-					
-					$('.selected').removeClass('selected');
-					$('.navbar-nav li a[href='+ thisHref +']').addClass('selected');
-					
-				}
-			});
-			
-			
-			//If we're at the bottom of the page, move pointer to the last section
-			bottomPage	= parseInt($(document).height()) - parseInt($(window).height());
-			
-			if(scrollPosition == bottomPage || scrollPosition >= bottomPage) {
-			
-				$('.selected').removeClass('selected');
-				$('navbar-nav li a:last').addClass('selected');
-			}
-		});
-		
+	    $('#GoToHome, .GoToto, #GoToWorks, #GoToFeatures, #GoToTeam, #GoToGallery, #GoToContact').scrollTo({ speed: 1400 });
+
+	    //Bootstraping variable
+	    headerWrapper = parseInt($('.navbar').height());
+	    offsetTolerance = 84;
+
+	    //Detecting user's scroll
+	    $(window).scroll(function() {
+
+	        //Check scroll position
+	        scrollPosition = parseInt($(this).scrollTop());
+
+	        //Move trough each menu and check its position with scroll position then add selected class
+	        $('.navbar-nav li a').each(function() {
+	            let thisHref = $(this).attr('href');
+	            let offset = $(thisHref).offset();
+
+	            if (offset) {
+	                thisTruePosition = parseInt($(thisHref).offset().top);
+	                thisPosition = thisTruePosition - headerWrapper - offsetTolerance;
+
+	                if (scrollPosition >= thisPosition) {
+	                    $('.selected').removeClass('selected');
+	                    $('.navbar-nav li a[href=' + thisHref + ']').addClass('selected');
+	                }
+	            }
+	        });
+
+
+	        //If we're at the bottom of the page, move pointer to the last section
+	        bottomPage = parseInt($(document).height()) - parseInt($(window).height());
+
+	        if (scrollPosition == bottomPage || scrollPosition >= bottomPage) {
+
+	            $('.selected').removeClass('selected');
+	            $('navbar-nav li a:last').addClass('selected');
+	        }
+	    });
+
 	});
