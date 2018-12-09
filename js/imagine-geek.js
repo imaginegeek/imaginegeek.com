62eb2e028c8ab955f50e79f5bfbe2ee371f21dba
Nicholas Abrams Work <nicholasabramswork@Nicholass-MacBook-Pro.local>
2018-12-09 12:21:15 -0500

    Major improvements to site, screen sizes fixed just needs gallery and content finalized.

--- js/imagine-geek.js
+++ js/imagine-geek.js
@@ -1,4 +1,6 @@
 $(function() {
+    var $formErrorsOutput = $('#form-errors');
+
     function submitUserInput(
         input,
         endpoint = 'https://docs.google.com/forms/d/e/1FAIpQLScT5_-BPvZPn1G0IpQPticPFk8yoG8KTPqXwkDjmZlMkpsq2A/formResponse'
@@ -51,8 +53,49 @@ $(function() {
         return payload;
     };
 
-    function filterFormControlsParseValues(valuesMap) {
-        var $control = $(this);
+    function fieldValidate(fieldname, value) {
+        var regExp;
+        var errorMessage;
+        var valid;
+        var required;
+        var descriptor;
+
+        function formatErrorMessage(descriptor, errorDetail) {
+            return 'Your ' + descriptor + ' ' + errorDetail + ' ';
+        }
+
+        switch (fieldname) {
+            case 'firstName':
+                regExp = /^[a-z ,.'-]+$/i;
+                errorMessage = 'is an invalid first name.';
+                required = true;
+                descriptor = 'first name';
+                break;
+            case 'lastName':
+                regExp = /^[a-z ,.'-]+$/i;
+                errorMessage = 'is an invalid last name.';
+                required = true;
+                descriptor = 'last name';
+                break;
+            case 'emailAddress':
+                regExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+                errorMessage = 'is an invalid email address.';
+                required = true;
+                descriptor = 'email address';
+                break;
+        }
+
+        if (!value && required) {
+            return formatErrorMessage(descriptor, 'cannot be blank.');
+        } else if (typeof regExp === 'object') {
+            valid = regExp.test(value);
+        }
+
+        // Allow non-required fields through and send any required field errors back to UI as formatted error.
+        return (valid === true || valid === undefined) || formatErrorMessage(descriptor, errorMessage);
+    }
+
+    function getControlValueWithNameAndType($control) {
         var type = $control.attr('type');
         var name = $control.attr('name');
         var value;
@@ -67,32 +110,84 @@ $(function() {
                 value = $control.val();
                 break;
             default:
-                {
-                    switch ($control.get(0).tagName.toLowerCase()) {
-                        case 'select':
-                        case 'textarea':
-                            value = $control.val();
-                            break;
-                        default:
-                            value = $control.html();
-                    }
+                switch ($control.get(0).tagName.toLowerCase()) {
+                    case 'select':
+                    case 'textarea':
+                        value = $control.val();
+                        break;
+                    default:
+                        value = $control.text();
                 }
         }
 
-        valuesMap[name] = value;
+        return {
+            name: name,
+            value: value,
+            type: type
+        };
+    }
 
-        return valuesMap;
+    function handleValidationErrorsEvent(errorsObj) {
+        var errorMessageUi = '';
+        for (var fieldname in errorsObj) {
+            if (!errorsObj.hasOwnProperty(fieldname)) {
+                return;
+            }
+
+            if (errorsObj[fieldname]) {
+                errorMessageUi += errorsObj[fieldname];
+            }
+        }
+
+        return errorMessageUi;
     }
 
     function sendMappedUserInputFromUi() {
         var payload = {};
+        var errors = {};
+        var controlValueWithNameAndType;
+        var type;
+        var errorsEncountered;
+        var $control;
+        var name;
+        var value;
 
         $('#quote-form').find('input, textarea, select').each(function() {
-            payload = filterFormControlsParseValues.call(this, payload);
+            var errorMessage;
+            $control = $(this);
+            controlValueWithNameAndType = getControlValueWithNameAndType($control);
+            name = controlValueWithNameAndType.name;
+            type = controlValueWithNameAndType.type;
+            value = controlValueWithNameAndType.value;
+
+            // Either true for no validation errors, or a string of errors found if invalid.
+            valid = fieldValidate(name, value);
+
+            if (valid === true) {
+                payload[name] = value;
+            } else {
+                errorMessage = valid;
+                errors[name] = errorMessage;
+                errorsEncountered = true;
+            }
         });
 
-        submitUserInput(mapInputToPayload(payload));
+        if (errorsEncountered) {
+            togglePageSection('form-errors', -100, 350);
+            $formErrorsOutput.text(handleValidationErrorsEvent(errors));
+        } else {
+            $formErrorsOutput.text('');
+            submitUserInput(mapInputToPayload(payload));
+        }
+
+        return !errorsEncountered;
     }
+
+    $('[name="firstName"], [name="lastName"], [name="emailAddress"]').on('keyup', function() {
+        if (window.submitAttempted) {
+            sendMappedUserInputFromUi();
+        }
+    });
 
     function displayConfirmation() {
         var $quoteSubmitButton = $('[name="quoteSubmit"]');
@@ -110,12 +205,12 @@ $(function() {
         }, 1750);
     }
 
-    function togglePageSection(sectionName, offset) {
+    function togglePageSection(sectionName, offset, timing) {
         var $section = $(`#${sectionName}`);
 
         $('html, body').animate({
             scrollTop: $section.offset().top
-        }, 2000);
+        }, timing);
     }
 
     function toggleNavSelectionFromClick(navLinkSelectors) {
@@ -144,8 +239,12 @@ $(function() {
     $('#quote-form').submit(function(event) {
         event.stopPropagation();
         event.preventDefault();
-        sendMappedUserInputFromUi();
-        displayConfirmation();
+        if (!window.submittedAttempted) {
+            window.submitAttempted = true;
+        }
+        if (sendMappedUserInputFromUi()) {
+            displayConfirmation();
+        }
     });
 
     // Bind getQuotes section display
@@ -157,6 +256,6 @@ $(function() {
         });
 
         event.stopPropagation();
-        togglePageSection('quote-section', -100);
+        togglePageSection('quote-section', -100, 2000);
     });
 });
